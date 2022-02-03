93d6b3f78058594efec0dc2d67038f43d53c0946
Lavcodes <sanjali_b@ce.iitr.ac.in>
2022-02-03 21:26:16 +0530

    changes

--- src/app/components/Dashboard/ProfileNew/index.js
+++ src/app/components/Dashboard/ProfileNew/index.js
@@ -1,19 +1,59 @@
 import React from 'react';
 
 import ProfileHeader from './ProfileHeader';
+import Details from './Details'
+
 
 import './css/index.css'
 
 const ProfileNew = () => {
+
+    const data = [
+        {
+            detailName : 'Name',
+            detailValue : 'Pramegh Uikey'   
+        },
+        {
+            detailName : 'CA Rank',
+            detailValue : '12th Rank'   
+        },
+        {
+            detailName : 'Email',
+            detailValue : 'abcd@gmail.com'   
+        },
+        {
+            detailName : 'Mobile No.',
+            detailValue : '01234 56789'   
+        },
+        {
+            detailName : 'College',
+            detailValue : 'IIT ROORKEE'   
+        },
+        {
+            detailName : 'Address',
+            detailValue : 'B14, Type 4, Krishi Kunj Colony, Bajaj Nagar, City,State'   
+        },
+        {
+            detailName : 'Registrations by your CA-ID',
+            detailValue : '18 People'   
+        }
+
+
+    ]
+
     return (
-        <div className='profile-container'>
-            <div className='profile-header'>
+        <div className='profile-container p-0 m-0 h-100'>
+            <div className='row profile-header'>
                 <ProfileHeader points='850'></ProfileHeader>
             </div>
             <div className='row profile-component'>
 
-                <div className='col-md-6 details-component'></div>
-                <div className='col-md-6 delegates-registered'></div>
+                <div className='col-md-6 details-component'>
+                    <Details></Details>
+                </div>
+                <div className='col-md-6 delegates-registered'>
+
+                </div>
             </div>
 
         </div>
