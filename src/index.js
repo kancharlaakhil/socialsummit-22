669c6612debd5d78a9d22af7680b61ac1952cdf9
Lavcodes <sanjali_b@ce.iitr.ac.in>
2022-02-14 23:53:34 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ import store from './redux/store'
 
 import axios from 'axios';
 
-axios.defaults.baseURL = "http://127.0.0.1:3000/api"
-//axios.defaults.baseURL = "https://api.socialsummit.iitr.ac.in/api"
+//axios.defaults.baseURL = "http://127.0.0.1:3000/api"
+axios.defaults.baseURL = "https://api.socialsummit.iitr.ac.in/api"
 
 //axios.defaults.baseURL = "https://api.socialsummit.iitr.ac.in/api"
 
