d6f0376b7977e44832320bdf137bc0289ca4575b
IamArnab01 <imarnab01@gmail.com>
2023-02-04 23:32:10 +0530

    changes

--- src/app/components/Home/index.js
+++ src/app/components/Home/index.js
@@ -1,20 +1,19 @@
-import "./css/home.css"
+import React from "react";
 import Hometop from "./Hometop/Main";
 import Events from "./Events/Events";
 import Footfall from "./Footfalls";
 import PastSpeakers from "./PastSpeakers/Speakers";
-import { useWidthAndHeight } from "../../../hooks/useWidthAndHeight"
+import { useWidthAndHeight } from "../../../hooks/useWidthAndHeight";
 import Testimonials from "./Testimonials";
-import "./css/slider.css"
-
+import "./css/slider.css";
+import "./css/home.css";
 const HomeMain = () => {
   let [width] = useWidthAndHeight();
   // returns the screen/window width.
   // Very useful tacking screen specific tasks within react component.
   // Can be used only within functional components
   return (
-    <div className="homeContainer"> 
-
+    <div className="homeContainer">
       <div className="th-home-top-container">
         <Hometop width={width} />
 
