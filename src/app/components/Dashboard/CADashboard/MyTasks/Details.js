97318e41e64d50df57cc6487acfe5034d7e57044
IamArnab01 <imarnab01@gmail.com>
2023-02-04 23:43:05 +0530

    change

--- src/app/components/Dashboard/CADashboard/MyTasks/Details.js
+++ src/app/components/Dashboard/CADashboard/MyTasks/Details.js
@@ -1,103 +1,87 @@
-import React from 'react';
+import React from "react";
 
-
-import './css/details.css'
+import "./css/details.css";
 
 const singleDetail = (detailName, detailValue) => {
+  const color = detailName === "Points to gain" ? "#663AA3" : "#444444";
+  const weight = detailName === "Points to gain" ? "600" : "normal";
 
-    const color = detailName === 'Points to gain' ? '#663AA3' : '#444444';
-    const weight = detailName === 'Points to gain' ? '600' : 'normal'
+  return (
+    <div className="row single-detail">
+      <div className="col-3">{detailName}</div>
+      <div className="col-1"> : </div>
+      <div
+        className="col-8"
+        style={{
+          color: `${color}`,
+          fontWeight: `${weight}`,
+        }}
+      >
+        {detailValue}
+      </div>
+    </div>
+  );
+};
 
-    
+const Details = ({ tasks, index }) => {
+  const getMonth = (month) => {
+    if (month === "01") return "Jan";
+    else if (month === "02") return "Feb";
+    else if (month === "03") return "Mar";
+    else if (month === "04") return "Apr";
+    else if (month === "05") return "May";
+    else if (month === "06") return "Jun";
+    else if (month === "07") return "Jul";
+    else if (month === "08") return "Aug";
+    else if (month === "09") return "Sep";
+    else if (month === "10") return "Oct";
+    else if (month === "11") return "Nov";
+    else if (month === "12") return "Dec";
+  };
 
-    return (
-        <div className='row single-detail'>
-                <div className='col-3'>{detailName}</div>
-                <div className='col-1'> : </div>
-                <div className='col-8'
-                    style={{
-                        color : `${color}`,
-                        fontWeight : `${weight}`
-                    }}
-                
-                >{detailValue}</div>
-          </div>
-    )
-}
+  const convertDate = (date) => {
+    const d = date?.split("T")[0];
+    const use = d?.split("-");
+    if (use) {
+      const month = getMonth(use[1]);
+      const day = use[2];
+      const year = use[0];
 
-const Details = ({tasks, index}) => {
+      const finalDate = `${day} ${month} ${year}`;
 
-    const getMonth = (month) => {
-        if(month==='01') return 'Jan'
-        else if (month ==='02') return 'Feb'
-        else if (month ==='03') return 'Mar'
-        else if (month ==='04') return 'Apr'
-        else if (month ==='05') return 'May'
-        else if (month ==='06') return 'Jun'
-        else if (month ==='07') return 'Jul'
-        else if (month ==='08') return 'Aug'
-        else if (month ==='09') return 'Sep'
-        else if (month ==='10') return 'Oct'
-  
-        else if (month ==='11') return 'Nov'
-        else if (month ==='12') return 'Dec'
-      }
-
-    const convertDate = (date) => {
-        const d = date?.split('T')[0];
-        const use = d?.split('-');
-        if(use){
-            const month = getMonth(use[1]);
-            const day = use[2];
-            const year = use[0];
-    
-            const finalDate = `${day} ${month} ${year}`
-    
-            return finalDate
-        }
-       
+      return finalDate;
     }
+  };
 
-    
+  const issuedDate = convertDate(tasks[index]?.issuedDate);
+  const deadlineDate = convertDate(tasks[index]?.deadlineDate);
 
-    
+  const details = [
+    {
+      detailName: `Task #${index + 1}`,
+      detailValue: tasks[index]?.description,
+    },
+    {
+      detailName: "Date Issued",
+      detailValue: issuedDate,
+    },
+    {
+      detailName: "Deadline",
+      detailValue: deadlineDate,
+    },
+    {
+      detailName: "Points to gain",
+      detailValue: `${tasks[index]?.totalPoints} points (${tasks[index]?.eachPoints} each)`,
+    },
+  ];
 
-    const issuedDate = convertDate(tasks[index]?.issuedDate);
-    const deadlineDate = convertDate(tasks[index]?.deadlineDate)
+  return (
+    <div className="details-component">
+      {details.map((ele) => {
+        return singleDetail(ele.detailName, ele.detailValue);
+      })}
+    </div>
+  );
+};
 
-    const details = [
-        {
-            detailName : `Task #${index+1}`,
-            detailValue : tasks[index]?.description
-        },{
-            detailName : 'Date Issued',
-            detailValue : issuedDate 
-        },
-        {
-            detailName : 'Deadline',
-            detailValue : deadlineDate 
-        },
-        {
-            detailName : 'Points to gain',
-            detailValue : `${tasks[index]?.totalPoints} points (${tasks[index]?.eachPoints} each)`  
-        }
-
-    ]
-
-
-   
-    return(
-        <div className='details-component'>
-            {
-                details.map((ele) => {
-                    return singleDetail(ele.detailName, ele.detailValue)
-                })
-            }
-
-        </div>
-    )
-}
-
-
-  
-  export default Details;
+export default Details;
