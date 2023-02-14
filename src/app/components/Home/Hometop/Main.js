2922c3025b13e47fddcdc3bf4158e2b4f0c4141d
Ayush Saini <101219576+Ayushsaini076@users.noreply.github.com>
2023-02-14 16:37:24 +0530

    changes in hometop

--- src/app/components/Home/Hometop/Main.js
+++ src/app/components/Home/Hometop/Main.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Main.css"
+import downarrow from './downarrow.png'
 
 function Main() {
   return (
@@ -9,7 +10,7 @@ function Main() {
         </div>
         <div className="cont">
             <span className='date'>11th - 12th March 2023</span>
-            <p>Take the next step forward with National Social Summit <br /> 2022 and dabble in the color of inclusion.</p>
+            <p>Take the next step forward with National Social Summit <br /> 2023 and dabble in the color of inclusion.</p>
         </div>
         <div className="btnbox">
             <button className='btn2'>
@@ -18,6 +19,12 @@ function Main() {
             <button  className='btn3'>
               <a className='plink' href="https://secure-ptg.payphi.com/pg/portal/pay/initiatePayOrder?merchantID=P_30861" target="_blank" rel="noopener noreferrer">Payment</a></button>
         </div>
+        <div className='cont'>
+        <p >Slide down to know more</p>
+        <img  src={downarrow} alt="" />
+
+        </div>
+
         
       
     </div> 
