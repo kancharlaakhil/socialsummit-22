3edf11ae4b4e704487dd220a87a59e63c0c6f4d5
Prakhar Khatri <103585489+codeprakhar25@users.noreply.github.com>
2023-12-16 10:55:06 +0530

    footer (#107)
    Co-authored-by: ganesh prajapati <ganeshprajapati1042001@gmail.com>

--- src/app/layouts/Footer/ContactUs.js
+++ src/app/layouts/Footer/ContactUs.js
@@ -1,85 +1,75 @@
 import React from "react";
 import "./ContactUs.css";
-import im1 from "./images/Logo2.png";
-import im2 from "./images/facebook.png";
-import im3 from "./images/inst.png";
-import im4 from "./images/Linkedin.png";
+import im1 from "./images/logo2.svg";
+import im2 from "./images/Vector.png";
+import im3 from "./images/Instagram.png";
+import im4 from "./images/LinkedIn.png";
 import im5 from "./images/Twitter.png";
-
+import im6 from "./images/Gmail.png";
 function ContactUs() {
   return (
     <div className="Contact-container">
-      <div className="contact-main">
-        <div className="contactleft">
-          <div className="socialmain">
-            <img src={im1} alt="" />
-            <h2 className="hd1">BREAKING CONVENTIONS</h2>
-            <p>
-              <a href="mailto:socialsummit@iitr.ac.in">
-                socialsummit@iitr.ac.in
-              </a>
-            </p>
-            <p>+91 8791101131</p>
-
-            <p>Follow us on</p>
-            <div className="socialicons">
-              <img src={im2} alt="">
-                {/* <a href="https://www.instagram.com/iitrsocialsummit/?utm_medium=copy_link"></a> */}
-              </img>
-              <img src={im3} alt="" />
-              <img src={im4} alt="" />
-              <img src={im5} alt="" />
+      <div className="footerCol1">
+        <div className="footerLogo">
+          <div className="logoImg">
+            <img src={im1} />
+          </div>
+          <div className="logoContaint">
+            <div className="NSS">
+              <p>
+                National<br />
+                Social<br />
+                Summit
+              </p>
+            </div>
+            <div className="date-box">
+              <p>17th-19th Feb 2024</p>
             </div>
           </div>
         </div>
-        <div className="contactright">
-          <h4>Contact Us</h4>
-          <div>
-            <form action="https://formspree.io/f/xyyakoor" method="POST">
-              <div className="form">
-                <div className="formcss">
-                  <input
-                    name="Name"
-                    className="inputbox"
-                    type="text"
-                    placeholder="Name"
-                  />
-                  <input
-                    name="Number"
-                    className="inputbox"
-                    type="text"
-                    placeholder="Contact"
-                  />
-                  <input
-                    name="Email"
-                    className="inputbox"
-                    type="email"
-                    placeholder="Email ID"
-                  />
-                </div>
-
-                <textarea
-                  name="Message"
-                  className="textarea"
-                  id=""
-                  cols="30"
-                  rows="6"
-                >
-                  Message
-                </textarea>
-              </div>
-
-              <button type="submit" className="formbutton">
-                Send Message
-              </button>
-            </form>
-          </div>
+        <div className="footerText">
+          <p>
+            It is a platform to come together to address the socio-economic problems persisting in the country. It brings together the socially active groups present all across the country.
+          </p>
+        </div>
+        <div className="contactusBtn">
+          <button type="button" class="btn btn-primary">Contact Us</button>
         </div>
       </div>
-
-      <div className="footer">
-        <hr />
-        <p className="copyright">@copyright | National Social Summit</p>
+      <div className="footerCol2">
+        <div className="col1">
+          <h4>About</h4>
+          <a>Our History</a>
+          <a>Blogs</a>
+          <a>Privacy Policy</a>
+          <a>Terms & Conditions</a>
+        </div>
+        <div className="col1">
+          <h4>Information</h4>
+          <a>Site Map</a>
+          <a>FAQs</a>
+          <a>Privacy Policy</a>
+          <a>Terms & Conditions</a>
+        </div>
+      </div>
+      <div className="footerCol3">
+        <div className="icon-box">
+          <div className="icon">
+            <a href="/"> <img src={im5} /></a>
+          </div>
+          <div className="icon">
+            <a href="/"> <img src={im4} /></a>
+          </div>
+          <div className="icon">
+            <a href="/"> <img src={im3} /></a>
+          </div>
+          <div className="icon">
+            <a href="/"> <img src={im2} /></a>
+          </div>
+          <div className="icon">
+            <a href="/"> <img src={im6} /></a>
+          </div>
+        </div>
       </div>
     </div>
   );
