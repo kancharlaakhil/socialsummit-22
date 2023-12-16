3fa25170b0f120cfba6390d672a4ea9ed0c731ba
Prakhar Khatri <103585489+codeprakhar25@users.noreply.github.com>
2023-12-16 16:36:11 +0530

    About-Us (#109)
    * footer
    
    * aboutpage
    
    ---------
    
    Co-authored-by: ganesh prajapati <ganeshprajapati1042001@gmail.com>

--- src/app/components/About/OurStory.js
+++ src/app/components/About/OurStory.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import './OurStory.css'
-import aboutimg from './assets/Web_about us.png'
-import maimg from './assets/Mobile_about us.png'
-
+import aboutimg from './assets/aboutbg.png'
+import aboutimgcover from './assets/aboutbgcover.png'
+import aboutbg2 from './assets/aboutbg2.png'
+import aboutsecond from './assets/aboutsecond.svg'
 function OurStory() {
   return (
-    <div className='ourstory-container'>
-      <div className="images">
-        <img className='aimg' src={aboutimg} alt="" />
-        <img className='maimg' src={maimg} alt="" />
-      
+    <div className='About-container'>
+      <div className="ourstory">
+        <img src={aboutimg} className='aboutbg' />
+        <img src={aboutimgcover} className='aboutbgcover' />
+        <div className="about-containt">
+          The National Social Summit, organized by the National Service Scheme(NSS) IIT Roorkee was established in 2016. It is a platform to come together to address the socio-economic problems persisting in the country. It brings together the socially active groups present all across the country. This social-fest includes various events such as Panel Discussions, Case Studies, Social Drama, Workshops, Model United Nations and Cultural shows.
+        </div>
       </div>
-      <div className="content">
-        <h3 className='hd1'>OUR STORY</h3>
-        <p className='para'>The National Social Summit, organized by the National Service Scheme(NSS) IIT Roorkee was established in 2016. It is a platform to come together to address the socio-economic problems persisting in the country. It brings together the socially active groups present all across the country. This social-fest includes various events such as Panel Discussions, Case Studies, Social Drama, Workshops, Model United Nations and Cultural shows.</p>
+      <div className="ourstorypage2">
+        <img src={aboutbg2} className='aboutbg2' />
+        <div className="components">
+          <img src={aboutsecond} className='aboutsecond' />
+          <div className="about-containt2">
+            <p>
+              We are a dedicated workforce of over 1000 volunteers striving selflessly for the growth of our nation. This year, we unite the youth of the nation to be the fire that burns the foundation of prejudices and disparity in our society. National Social Summit 2023 envisions an inclusive society for ourselves and our posterity. Today, the Indian society falls short due to the prevalence of prejudices, bigotry and disproportionate representation. This year we seek to reform these societal values and bring abouvt the much needed change. The growing unrest in the society gives us a premonition about our grim future in a voice that is dark and foreboding. With the soaring disparities in the current global scenario we seem ill prepared to deal with the challenges. Hence, the National Social Summit is here to highlight these issues at a national platform in order to envision an inclusive society.
+            </p>
+          </div>
+        </div>
       </div>
-    
-      
-  
+
+
     </div>
   )
 }
--- src/app/components/About/index.js
+++ src/app/components/About/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import OurStory from "./OurStory";
-import Dynamics from "./Dynamics"
-import VandM from "./VandM"
+
 
 
 function About() {
   return (
-    <React.Fragment>
+
       <OurStory/>
-      <Dynamics/>
-      <VandM/>
-    </React.Fragment>
+     
+ 
 
 );
 }
